19d0f256678828f42a6626d411130456de4815d2
alexidev23 <alexisescobar1234@gmail>
2023-03-06 20:59:13 -0300

    modal para proyectos y correccion de errores

--- js/script.js
+++ js/script.js
@@ -13,3 +13,49 @@ function activeMenu() {
 activeMenu()
 
 window.addEventListener("scroll", activeMenu)
+
+
+//Modal
+
+const modal = document.querySelector('dialog')
+const salir = document.getElementById('salir')
+const abrir = document.getElementById('abrir')
+
+abrir.addEventListener('click', () => modal.showModal())
+salir.addEventListener('click', () => modal.close())
+
+// Prueba de informacion para portafolio
+
+const proyectos = [
+    {
+        "titulo": "Calculadora",
+        "tecnologias": ["html","css","javascript"],
+        "descripcion": "Este es mi primer proyecto, se trata de unacalculadora que resulve operaciones simples",
+        "img": ""
+    },
+
+    {
+        "titulo": "Portafolio - Entrega final",
+        "tecnologias": ["html","css","javascript"],
+        "descripcion": "Este es mi primer proyecto, se trata de unacalculadora que resulve operaciones simples",
+        "img": ""
+    },
+
+    {
+        "titulo": "Encriptador de texto",
+        "tecnologias": ["html","css","javascript"],
+        "descripcion": "Este es mi primer proyecto, se trata de unacalculadora que resulve operaciones simples",
+        "img": ""
+    },
+
+    {
+        "titulo": "Pagina web - Pasteleria",
+        "tecnologias": ["html","css","javascript"],
+        "descripcion": "Este es mi primer proyecto, se trata de unacalculadora que resulve operaciones simples",
+        "img": ""
+    }
+]
+
+for (x of proyectos) {
+    console.log(x.titulo)
+}
