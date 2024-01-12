121a6ae63ad213515bab8d4b6e772dd6986e3b87
AlexisDev <alexisescobar1234@gmail.com>
2024-01-12 14:03:40 -0300

    correcion de estilos para vista mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,16 +2,17 @@ import React, { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import { Links } from './Links'
 import Logo from '../../assets/img/iconito.svg'
+import { Link } from 'react-scroll'
 
 export function Navbar () {
   const [nav, setNav] = useState(false)
   return (
-    <div className='flex justify-between items-center md:w-full w-full h-20 fixed px-4 bg-background z-50'>
-      <div className='md:mx-16 mx-4'>
-        <a href='Home' className='md:text-3xl text-2xl font-semibold text-links flex items-center'>
+    <div className='flex justify-between items-center w-full h-20 fixed px-4 bg-background z-50'>
+      <div className='lg:mx-16 mx-4'>
+        <Link to='Home' smooth duration={500} className='md:text-3xl text-2xl font-semibold text-links flex items-center cursor-default'>
           <img src={Logo} alt='Logo AlexisDev' className='w-10' />
           AlexisDev
-        </a>
+        </Link>
       </div>
 
       <ul className='hidden xl:flex mx-16'>
@@ -30,7 +31,7 @@ export function Navbar () {
       </div>
 
       {nav && (
-        <ul className='flex flex-col justify-center items-center absolute top-20 left-0 w-full h-96 gap-10 text-links bg-background'>
+        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen gap-10 text-links bg-background'>
           <Links />
         </ul>
       )}
