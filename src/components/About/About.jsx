7b5980ec24c6775b8ba1147e8126fe9b74a8f19b
AlexisDev <alexisescobar1234@gmail.com>
2024-06-27 09:29:09 -0300

    delete component and new desing section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
-import Perfil from '../../assets/img/foto5.webp'
-import { Skills } from './Skills'
+import Perfil from '../../assets/foto5.avif'
+import { IconUser } from '../Icons/IconUser'
 
 export function About () {
   return (
     <section
       id='sobreMi'
-      className='py-20 flex items-center flex-col justify-center sm:w-minSection sm:m-auto lg:w-[800px] xl:h-screen'
+      className='py-20 flex flex-col sm:w-minSection sm:m-auto lg:w-[800px] xl:h-screen'
     >
-      <h2 className='text-center text-4xl font-bold uppercase text-links'>Sobre mi</h2>
-      <div className='flex w-auto pt-10'>
-        <div>
-          <img src={Perfil} alt='Foto de Alexis Escobar' className='hidden lg:flex lg:rounded-large lg:w-96' />
-        </div>
-        <div className='w-full mx-8 my-5 text-balance lg:ml-4 lg:mr-0 lg:px-10'>
+      <div className='flex gap-2'>
+        <IconUser />
+        <h2 className='text-4xl font-bold uppercase text-links'>Sobre mi</h2>
+      </div>
+      <div className='flex w-auto pt-6'>
+        <div className='w-full text-balance'>
           <p className='text-white font-medium text-xl'>
             Soy una persona apasionada al aprendizaje y nuevas tecnologías, actualmente estoy estudiando una tecnicatura en programación y estoy en busca de trabajo donde pueda demostrar mis habilidades y pueda adquirir experiencia y nuevos conocimientos.
           </p>
-          <div>
-            <h5 className='text-center text-3xl font-bold text-links mt-6'>Skills</h5>
-            <Skills />
-          </div>
+        </div>
+        <div>
+          <img src={Perfil} alt='Foto de Alexis Escobar' className='hidden lg:flex rounded-3xl lg:w-80' />
         </div>
       </div>
     </section>
