1b1a74168fb157c5d0945d9f877516523c867c5b
AlexisDev <alexisescobar1234@gmail.com>
2024-06-27 08:37:04 -0300

    add file constants

---
+++ src/constants/constants.js
@@ -0,0 +1,47 @@
+import Linkedin from '../assets/linkedin.svg'
+import GitHub from '../assets/github.svg'
+import Curriculum from '../assets/file.svg'
+
+export const linksNavBar = [
+  {
+    id: 1,
+    url: '#experiencia',
+    name: 'Experiencia'
+  },
+  {
+    id: 2,
+    url: '#proyectos',
+    name: 'Proyectos'
+  },
+  // {
+  //   id: 3,
+  //   url: '#sobreMi',
+  //   name: 'Sobre Mi'
+  // },
+  {
+    id: 3,
+    url: '#contacto',
+    name: 'Contacto'
+  }
+]
+
+export const LinksMedialSocial = [
+  {
+    id: 1,
+    name: 'Linkedin',
+    icon: Linkedin,
+    url: 'https://www.linkedin.com/in/alexis-escobar-95b491184/'
+  },
+  {
+    id: 2,
+    name: 'Github',
+    icon: GitHub,
+    url: 'https://github.com/alexidev23'
+  },
+  {
+    id: 3,
+    name: 'Curriculum',
+    icon: Curriculum,
+    url: 'https://drive.google.com/file/d/1ljjixiNoLn83KpG_Jk-etX_cxL-ufBOi/view?usp=sharing'
+  }
+]
